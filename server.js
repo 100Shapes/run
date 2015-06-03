6206b51721356d2ab1c767aacf050feecb0e71f7
Paolo Memoli <paolo@100shapes.com>
2015-06-03 02:58:49 +0200

    refactored

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({ port: 8080 });
 
-var backend = require('./routes/backend')(server);
-var database = require('./routes/database')(server);
-var frontend = require('./routes/frontend')(server);
-var bluetooth = require('./bluetooth')(server);
+var backend = require('./server/backend')(server);
+var methods = require('./server/methods')(server);
+var frontend = require('./server/frontend')(server);
+var bluetooth = require('./server/bluetooth')(server);
 var Path = require('path');
 
 server.start(function() {
